3e4e8cca75cb2ac0507dc259c801144ccbec5ce0
dparmar <116805076+dparmar04@users.noreply.github.com>
2025-05-17 16:33:11 +0530

    animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,27 +3,25 @@ import Navbar from "./Navbar";
 import { gsap } from "gsap";
 import { ArrowRight } from 'lucide-react';
 
-const heroAnimation = (ref) => {
-  gsap.fromTo(ref.current, {
-    opacity: 0,
-    y: 100,
-  }, {
-    opacity: 1,
-    y: 0,
-    duration: 0.5,
-    stagger: 0.6,
-    ease: "power3.out",
-  });
-};
-
 const Hero = () => {
   const heroRef = useRef([]);
-  
+
   useEffect(() => {
-    if (heroRef.current.length === 3) {
-      heroAnimation(heroRef);
+    if (heroRef.current.length === 2) {
+      // Text animations
+      gsap.fromTo(heroRef.current.slice(0, 2), {
+        opacity: 0,
+        y: 80,
+      }, {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        stagger: 0.2,
+        ease: "power3.out",
+      });
     }
   }, []);
+
 
   return (
     <>
@@ -36,10 +34,10 @@ const Hero = () => {
             Sell Your Unused Licenses with SoftSell
           </h1>
           <p ref={(el) => (heroRef.current[1] = el)} className="mt-6 text-base sm:text-lg md:text-xl max-w-2xl"> Turn idle software into immediate cash. Secure, fast, and hassle-free with SoftSell.</p>
-          <button ref={(el) => (heroRef.current[2] = el)}
-            className="mt-8 bg-gradient-to-br from-[#ae7de8] to-[#947be4] text-black px-6 py-3 rounded-md font-semibold text-lg md:text-xl flex items-center gap-2 group hover:scale-105 transition-all duration-300"
+          <button className="mt-8 w-max bg-gradient-to-br from-[#ae7de8] to-[#947be4] text-black px-6 py-3 rounded-md font-semibold text-lg md:text-xl flex items-center gap-2 group hover:scale-105 transition-all duration-300"
           > Get a Quote
             <ArrowRight className="transform -translate-x-2 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition-all duration-300 ease-out" />
+
           </button>
         </div>
       </section>
